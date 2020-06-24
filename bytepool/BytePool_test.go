f0713b62aeb97b64305e4274f3058cbf0e6ae25c
soyum2222 <soyum2222@DESKTOP-R733AVJ.localdomain>
2020-06-24 18:40:25 +0800

    second touching fish

--- bytepool/BytePool_test.go
+++ bytepool/BytePool_test.go
@@ -8,18 +8,15 @@ import (
 func TestIdle(t *testing.T) {
 
 	p := BytePool{}
+
 	p.New(1024)
 
 	begin, end := p.idle(500)
 
 	if begin != 0 || end != 500 {
+		fmt.Println(begin, end)
 		t.Fail()
 	}
-
-	p.mark(begin, end)
-
-	fmt.Println(p.idle(500))
-
 
 }
 
@@ -28,10 +25,91 @@ func TestMark(t *testing.T) {
 	p := BytePool{}
 	p.New(1024)
 
-	p.mark(0, 64)
+	p.mark(0, 500)
 
-	begin, end := p.idle(64)
+	begin, end := p.idle(100)
+
+	if begin != 500 || end != 600 {
+		fmt.Println(begin, end)
+		t.Fail()
+	}
 
 	fmt.Println(begin, end)
+}
 
+func TestFree(t *testing.T) {
+	p := BytePool{}
+	p.New(1024)
+
+	b, err := p.Malloc(500)
+	if err != nil {
+		panic(err)
+	}
+
+	for k := range b {
+		b[k] = uint8(k)
+	}
+
+	if p.Free(b) != nil {
+		panic(err)
+	}
+
+	b, err = p.Malloc(500)
+	if err!=nil {
+		panic(err)
+	}
+
+	for k := range b {
+		if b[k] != uint8(k) {
+			t.Fail()
+		}
+	}
+
+}
+
+func BenchmarkBytePool(b *testing.B) {
+	pool := BytePool{}
+	pool.New(1 << 20)
+
+	for i := 0; i < b.N; i++ {
+
+		b, err := pool.Malloc(1 << 10)
+		if err != nil {
+			panic(err)
+		}
+
+		donothing(b)
+
+		c, err := pool.Malloc(1 << 10)
+		if err != nil {
+			panic(err)
+		}
+
+		donothing(c)
+
+		if err = pool.Free(b); err != nil {
+			panic(err)
+		}
+		if err = pool.Free(c); err != nil {
+			panic(err)
+		}
+	}
+
+}
+
+
+func BenchmarkNewByte(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		b := make([]byte, 1<<10)
+		donothing(b)
+
+	}
+
+}
+
+func donothing(b []byte) {
+	for k := range b {
+		b[k] = uint8(k)
+	}
 }
--- bytepool/bytePool.go
+++ bytepool/bytePool.go
@@ -3,22 +3,19 @@ package bytePool
 import (
 	"errors"
 	"math"
+	"unsafe"
 )
 
 type BytePool struct {
 	b []byte
-	m []int64
+	m []uint64
 }
 
 func (b *BytePool) New(size int64) {
 
 	b.b = make([]byte, size)
 
-	if size%64 == 0 {
-		b.m = make([]int64, (size / 64))
-	} else {
-		b.m = make([]int64, (size/64)+1)
-	}
+	b.m = make([]uint64, int(math.Ceil(float64(size)/float64(64))))
 
 }
 
@@ -30,29 +27,80 @@ func (b *BytePool) Malloc(size int64) ([]byte, error) {
 		return nil, errors.New("no suitable continuous space")
 	}
 
+	if end-begin != size {
+		return nil, errors.New("bad idle")
+	}
+
 	b.mark(begin, end)
 
 	return b.b[begin:end], nil
 
 }
 
-//func (b *BytePool) Free(s []byte) error {
-//
-//	addr1 := (*int64)(unsafe.Pointer(&b.b[0]))
-//	addr2 := (*int64)(unsafe.Pointer(&s[0]))
-//
-//	if *addr1 > *addr2 {
-//		return errors.New("not mine")
-//	}
-//
-//	offset := *addr2 - *addr1
-//
-//}
+func (b *BytePool) Free(s []byte) error {
+
+	addr1 := (uintptr)(unsafe.Pointer(&b.b[0]))
+
+	addr2 := (uintptr)(unsafe.Pointer(&s[0]))
+
+	if addr1 > addr2 {
+		return errors.New("not mine")
+	}
+
+	offset := int(addr2 - addr1)
+
+	if offset > len(b.b) {
+		return errors.New("not mine")
+	}
+
+	end := offset + len(s)
+
+	bit := offset % 64
+
+	for i := int(math.Ceil(float64(offset) / 64)); i < len(b.m); i++ {
+
+		for {
+
+			if bit%64 == 0 {
+
+				if end-bit >= 64 {
+					b.m[i] = 0
+					bit += 64
+					break
+				}
+
+				b.m[i] = b.m[i] &^ ((1 << ((end - bit) + 1)) - 1)
+				return nil
+
+			}
+
+			b.m[i] = b.m[i] & (^(1 << (bit % 64)))
+
+			bit++
+
+			if bit >= end {
+				return nil
+			}
+
+			if bit%64 == 0 {
+				break
+			}
+		}
+	}
+
+	return nil
+
+}
 
 func (b *BytePool) idle(size int64) (int64, int64) {
 	var bit int64
 
 	for i := 0; i < len(b.m); i++ {
+
+		if b.m[i] == (1<<64)-1 {
+			bit += 64
+			continue
+		}
 
 		for n := 0; n < 64; n++ {
 
@@ -63,13 +111,33 @@ func (b *BytePool) idle(size int64) (int64, int64) {
 
 				for j := i; j < len(b.m); j++ {
 
+					// 64bit batch and
+
+					// var end modulo to 64 == 0 ,bit pointer direction the b.m slice boundary
+					if end%64 == 0 {
+
+						remain := (size + begin) - end
+						if remain >= 64 && b.m[j]&((1<<64)-1) == 0 {
+							end += 64
+							continue
+						}
+
+						if remain < 64 && b.m[j]&((1<<(remain+1))-1) == 0 {
+							end += remain
+							return begin, end
+						}
+
+					}
+
+					// bit pointer not direction b.m slice boundary,then continuous offset pointer
 				loop:
 
-					if math.Ceil(float64(end)/float64(i+1)) > 64 {
+					if end/int64(j+1) == 64 {
 						continue
 					}
 
-					if b.m[j]&1<<end%int64(i+1) == 0 {
+					if b.m[j]&(1<<(end%64)) == 0 {
+
 						end++
 
 						if end-begin >= size {
@@ -78,15 +146,14 @@ func (b *BytePool) idle(size int64) (int64, int64) {
 
 						goto loop
 					} else {
+						bit = end
 						break
 					}
-
 				}
 
 			} else {
 				bit++
 			}
-
 		}
 	}
 
@@ -97,13 +164,25 @@ func (b *BytePool) mark(begin, end int64) {
 
 	index := begin / 64
 
-	bit := index % 64
+	var bit = begin - (index * 64)
 
 	size := end - begin
 
 	for i := index; i < int64(len(b.m)); i++ {
 
 	loop:
+
+		if bit == 0 {
+
+			if size > 64 {
+				b.m[i] = (1 << 64) - 1
+				size -= 64
+				continue
+			}
+
+			b.m[i] = (1 << (size)) - 1
+			return
+		}
 
 		b.m[i] = b.m[i] | (1 << bit)
 
